cac060a7fa7f938f19a3c20cda513a880d1fe566
artik292 <artik292@gmail.com>
2020-06-10 03:06:51 +0300

    Registraion update

--- ru/registration.php
+++ ru/registration.php
@@ -4,7 +4,7 @@ require '../app.php';
 
 $app = new App();
 
-$pac = new Stuff($db);
+$pac = new Patient($db);
 
 $reg = new \atk4\data\Model(new \atk4\data\Persistence_Array($a));
 $reg->addField('name',['type'=>'string','caption'=>'Имя']);
@@ -23,9 +23,6 @@ $form->buttonSave->set('Зарегистрироваться');
 $form->onSubmit(function($form) use($pac) {
 	if ($form->model['name'] == '') {
 		return $form->error('name', "Это поле обязательно для заполнения.");
-	}
-  if ($form->model['surname'] == '') {
-		return $form->error('surname', "Это поле обязательно для заполнения.");
 	}
   if ($form->model['phone_number'] == '') {
 		return $form->error('phone_number', "Это поле обязательно для заполнения.");
@@ -49,11 +46,11 @@ $form->onSubmit(function($form) use($pac) {
       return $form->error('email', "Данная электронная почта уже используется.");
   }
 
-//  $pac->tryLoadby('phone_number',$form->model['phone_number']);
+	$pac->tryLoadby('phone_number',$form->model['phone_number']);
 
-  // if(isset($pac->id)) {
-  //     return $form->error('phone_number', "Данный номер телефона уже используется.");
-  // }
+  	if(isset($pac->id)) {
+      return $form->error('phone_number', "Данный номер телефона уже используется.");
+  }
 
   switch ($form->model['password1']) {
     case 'password':
@@ -72,18 +69,16 @@ $form->onSubmit(function($form) use($pac) {
   }
 
   $email = $form->model['email'];
-	//$form->model->save();
-  $pac['name'] = $form->model['name'];
-  $pac['surname'] = $form->model['surname'];
-  //$pac['phone_number'] = $form->model['phone_number'];
-  //$pac['address'] = $form->model['address'];
-	$pac['status'] = 'admin';
+	$pac['name'] = $form->model['name']." ".$form->model['surname'];
+  $pac['phone_number'] = $form->model['phone_number'];
+  $pac['address'] = $form->model['address'];
   $pac['email'] = $form->model['email'];
   $pac['password'] = hash('sha256',$form->model['password1']);
   $pac->save();
   $pac->tryLoadby('email',$email);
   session_start();
   $_SESSION['id'] = $pac->id;
+	$_SESSION['status'] = 'patient';
 	//return $form->success('You were successfully registered');
   return new \atk4\ui\jsExpression('document.location = "main.php" ');
 });
