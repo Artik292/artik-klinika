7cadc1d9a2e0faadec19428f6d8d45897057090e
artik292 <artik292@gmail.com>
2020-06-10 03:08:04 +0300

    Main work. Thats why I commit it at 3 at night
    Вот тут вся суть, весь смыл проекта. Да, он вдохновлен проектом, который я уже делал. Но факт остаёться фактом, код мой

--- connection.php
+++ connection.php
@@ -22,22 +22,29 @@ class Spec extends \atk4\data\Model {
         parent::init();
         $this->addField('name',['caption'=>'title']);
         $this->addField('lang');
-        $this->hasMany('doctor');
+        $this->hasMany('Doctor');
+        $this->setOrder('name');
     }
 }
 
 class Doctor extends \atk4\data\Model {
     public $table = 'doctor';
+    public $title = 'Doctor';
+    public $name = 'Doctor';
     function init() {
         parent::init();
         $this->addField('name');
-        $this->addField('surname');
         $this->addField('hospital',['type'=>'text']);
+        $this->addField('room');
         $this->addField('email');
         $this->addField('password',['type'=>'password']);
         $this->hasMany('record');
         $this->hasMany('research');
+        $this->addField('available',['type'=>'boolean']);
         $this->hasOne('spec_id',new Spec());
+        //$this->hasOne('spec_id', [new Spec(),'caption'=>'Grupa'])->addTitle();
+        //$this->getElement('spec_id')->ui['visible'] = false;
+        $this->setOrder('name');
     }
 }
 
@@ -46,7 +53,6 @@ class Patient extends \atk4\data\Model {
     function init() {
         parent::init();
         $this->addField('name');
-        $this->addField('surname');
         $this->addField('email',['type'=>'email']);
         $this->addField('password',['type'=>'password']);
         $this->addField('phone_number');
@@ -60,10 +66,12 @@ class Record extends \atk4\data\Model {
     public $table = 'record';
     function init() {
         parent::init();
-        $this->addField('time',['type'=>'datetime']);
+        $this->addField('time');
+        $this->addField('patient_name');
         $this->addField('room');
-        $this->hasOne('doctor_id',new Doctor())->addTitle();
-        $this->hasOne('patient_id',new Patient())->addTitle();
+        $this->addField('available',['type'=>'boolean']);
+        $this->hasOne('doctor_id',new Doctor());
+        $this->hasOne('patient_id',new Patient());
     }
 }
 
@@ -77,3 +85,134 @@ class Research extends \atk4\data\Model {
         $this->hasOne('patient_id',new Patient());
     }
 }
+
+
+
+
+
+
+
+
+class Teacher extends \atk4\data\Model {
+    public $table = 'doctor';
+    public $title = 'Skolotājs';
+    public $name = 'Skolotājs';
+
+    function init() {
+        parent::init();
+
+        $this->addField('name',['caption'=>'Имя врача','required'=>TRUE]);
+        $this->addField('available',['type'=>'boolean']);
+
+        $this->addField('hospital',['type'=>'text']);
+        $this->addField('email');
+        $this->addField('password',['type'=>'password']);
+        $this->hasMany('research');
+        // $this->addField('cabinet',['caption'=>'Kabinets','required'=>TRUE]);
+        // $this->addField('available',['caption'=>'Būs','type'=>'boolean','required'=>TRUE]);
+        //
+        $this->hasOne('spec_id', [new Subject(),'caption'=>'Направление'])->addTitle();
+        //
+        // $this->getElement('subject_id')->ui['visible'] = false;
+        //
+        $this->hasMany('Vecaki', new Vecaki);
+        // $this->setOrder('name');
+
+    }
+}
+
+class Subject extends \atk4\data\Model {
+    public $table = 'spec';
+    public $title = 'Stunda';
+
+    function init() {
+        parent::init();
+
+        $this->addField('name',['caption'=>'Направление','required'=>TRUE]);
+
+        $this->hasMany('Teacher', new Teacher);
+        // $this->setOrder('name');
+    }
+}
+
+
+class Vecaki extends \atk4\data\Model {
+    public $table = 'record';
+    public $title = 'Vecāki';
+    public $name = 'parent_name';
+    public $title_field = 'parent_name';
+
+    function init() {
+        parent::init();
+
+        //$this->addField('student_name',['caption'=>'Skolnieka vārds un uzvārds','required'=>TRUE]);
+        //$this->addField('grade',['caption'=>'Klase','required'=>TRUE]);
+        //$this->addField('parent_name',['caption'=>'Vecāku vārds un uzvārds','required'=>TRUE]);
+        //$this->addField('contact_phone',['caption'=>'Kontaktnumurs','required'=>TRUE]);
+        $this->addField('patient_name');
+        $this->addField('room');
+        $this->hasOne('patient_id',new Patient());
+
+        $this->addField('available',['type'=>'boolean']);
+        $this->addField('time',['caption'=>'Время']);
+        $this->hasOne('doctor_id', new Teacher)->addTitle();
+        $this->setOrder('time');
+
+    }
+}
+
+// class Subject extends \atk4\data\Model {
+//     public $table = 'subject';
+//     public $title = 'Stunda';
+//
+//     function init() {
+//         parent::init();
+//
+//         $this->addField('name',['caption'=>'Grupa','required'=>TRUE]);
+//
+//         $this->hasMany('Teacher', new Teacher);
+//         $this->setOrder('name');
+//     }
+// }
+//
+// class Teacher extends \atk4\data\Model {
+//     public $table = 'teacher';
+//     public $title = 'Skolotājs';
+//     public $name = 'Skolotājs';
+//
+//     function init() {
+//         parent::init();
+//
+//         $this->addField('name',['caption'=>'Uzvārds, vārds','required'=>TRUE]);
+//         $this->addField('cabinet',['caption'=>'Kabinets','required'=>TRUE]);
+//         $this->addField('available',['caption'=>'Būs','type'=>'boolean','required'=>TRUE]);
+//
+//         $this->hasOne('subject_id', [new Subject(),'caption'=>'Grupa'])
+//             ->addTitle();
+//
+//         $this->getElement('subject_id')->ui['visible'] = false;
+//
+//         $this->hasMany('Vecaki', new Vecaki);
+//         $this->setOrder('name');
+//
+//     }
+// }
+// class Vecaki extends \atk4\data\Model {
+//     public $table = 'vecaki';
+//     public $title = 'Vecāki';
+//     public $name = 'parent_name';
+//     public $title_field = 'parent_name';
+//
+//     function init() {
+//         parent::init();
+//
+//         $this->addField('student_name',['caption'=>'Skolnieka vārds un uzvārds','required'=>TRUE]);
+//         $this->addField('grade',['caption'=>'Klase','required'=>TRUE]);
+//         $this->addField('parent_name',['caption'=>'Vecāku vārds un uzvārds','required'=>TRUE]);
+//         $this->addField('contact_phone',['caption'=>'Kontaktnumurs','required'=>TRUE]);
+//         $this->addField('time',['caption'=>'Laiks']);
+//         $this->hasOne('teacher_id', new Teacher)->addTitle();
+//         $this->setOrder('time');
+//
+//     }
+// }
---
+++ ru/main.php
@@ -0,0 +1,139 @@
+<?php
+
+require '../connection.php';
+
+session_start();
+
+if (!(isset($_SESSION['id']))) {
+  header('location: ../index.php');
+}
+
+require '../app.php';
+$app = new App();
+$to_record = $app->add(['Label','Просмотреть мои записи','blue left ribbon'])->link(['records']);
+
+$app->add(['ui'=>'hidden divider']);
+
+$logout = $app->add(['Label','Выйти ','red right ribbon','iconRight'=>'sign out'])->link(['../logout']);
+
+
+$app->add(['ui'=>'hidden divider']);
+
+$col = $app->add('Columns');
+$col->addClass('stackable');
+$subject= new Subject($db);
+$c1 = $col->addColumn();
+$c2 = $col->addColumn();
+$c3 = $col->addColumn();
+$c4 = $col->addColumn();
+$mes = $c4->add(['Message','Инструкция','massive info']);
+$mes->text->addParagraph('Выберите направление, затем врача и время. После нажмите "подтвердить".');
+
+$table_s = $c1->add(['Table','very basic selectable'])->addStyle('cursor', 'pointer');
+$table_s->setModel($subject, [$subject->title_field]);
+$table_s->on('click', 'tr', $c2->jsReload(['pr'=>$table_s->jsRow()->data('id')]));
+$pr = $app->stickyGet('pr');
+if ($pr) {
+  $subject->load($pr);
+  $doctor = $subject->ref('Teacher');
+  //$doctor->setOrder('name');
+  $table_t = $c2->add(['Table','very basic selectable'])->addStyle('cursor', 'pointer');
+  $table_t->setModel($doctor,[$doctor->title_field]);
+  $table_t->on('click', 'tr', $c3->jsReload(['t'=>$table_t->jsRow()->data('id')]));
+}
+$t = $app->stickyGet('t');
+if($t) {
+  $doctor= new Teacher($db);
+  $doctor = $doctor->load($t);
+  if ($doctor['available']) {
+    $parents=$doctor->ref('Vecaki');
+    $doctor_id = $doctor->id;
+    $parentss = new Vecaki($db);
+
+    $menu = $c3->add('Menu');
+    $menu->addClass('vertical fluid');
+    $menu->addHeader('Время ('.$doctor['name'].')');
+
+    $vir = $app->add('VirtualPage');
+    $vir->set(function($vir) use($parentss,$db,$app,$doctor_id,$t) {
+      $mes = $vir->add(['Message','Подтвердите выбор','massive info']);
+      $doctor = new Doctor($db);
+      $doctor->load($doctor_id);
+      $doctor_name = $doctor['name'];
+      $room = $doctor['room'];
+      $time = $app->stickyGet('time');
+      $specifik_id = $app->stickyGet('pr');
+      $specifik = new Spec($db);
+      $specifik->load($specifik_id);
+      $specifik = $specifik['name'];
+      $mes->text->addParagraph('Доктор: '.$doctor_name);
+      $mes->text->addParagraph('Направление: '.$specifik);
+      $mes->text->addParagraph('Дата: '.$time);
+      $form = $vir->add('Form');
+      $form->buttonSave->set('Подтвердить');
+      $form->onSubmit(function($form) use($app,$t,$db,$time,$room,$doctor_id) {
+        $record= new Record($db);
+        //$record->load($t);
+        $user = new Patient($db);
+        $user->load($_SESSION['id']);
+
+        $record['time'] = $time;
+        $record['room'] = $room;
+        $record['available'] = FALSE;
+        $record['doctor_id'] = $doctor_id;
+        $record['patient_id'] = $_SESSION['id'];
+        $record['patient_name'] = $user['name'];
+        $record->save();
+        return [$form->success('Запись прошла успешна.') , new \atk4\ui\jsExpression('document.location="main.php"')];
+
+      });
+    });
+
+    $min=0;
+    for ($hour=9;$hour<18;$hour++) {
+      for ($i=1;$i<=2;$i++) {
+        if ($min>=60) {
+          $min=0;
+        }
+        if($min<10) {
+          $time = $hour.':0'.$min;
+        }else {
+          $time = $hour.':'.$min;
+        }
+        $s=1;
+
+        if($parents->tryLoadAny()->loaded()) {
+          foreach($parents as $rowss) {
+            if ($rowss['time']==$time) {
+              $array=[$s=>$time];
+              $s=$s+1;
+            }
+          }
+          unset($rowss);
+          $check=FALSE;
+          for($n=0;$n<=$s;$n++){
+            if (isset($array[$n]) && $array[$n]==$time) {
+              $menu->addItem([$time,'disabled']);
+              $check=TRUE;
+            }
+          }
+          if($check==FALSE){
+            $menu->addItem($time)->on('click', new \atk4\ui\jsModal('Ieraksts',$vir,['time'=>$time]));
+          }
+          }else {
+            $menu->addItem($time)->on('click', new \atk4\ui\jsModal('Ieraksts',$vir,['time'=>$time]));
+          }
+        $min=$min+30;
+      }
+    }
+  } else {
+    $menu = $c3->add('Menu');
+    $menu->addClass('vertical fluid');
+    $menu->addHeader('Ceturtdien, vecāku dienas laikā, šīs skolotājas nebūs.');
+    //$menu->addItem('Šo skolotaja nebus.');
+  }
+}
+
+$app->add(['ui'=>'divider']);
+
+$app->add(['Label','Это приложение было создано Артуром Коневником ak19192 для LUDF ','green right ribbon'])->link('https://github.com/Artik292');
